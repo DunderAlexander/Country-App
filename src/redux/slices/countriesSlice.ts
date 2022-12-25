4e73fec8958118412df832d59ea2314d43ac1f76
DunderAlexander <mego.sanello@gmail.com>
2022-12-25 22:46:26 +0400

    working with boilerplate for slices,+first reducer

--- src/redux/slices/countriesSlice.ts
+++ src/redux/slices/countriesSlice.ts
@@ -1,1 +1,36 @@
 import { createSlice } from "@reduxjs/toolkit/dist/createSlice";
+import type { PayloadAction } from "@reduxjs/toolkit";
+
+// const [countriesList, setCountriesList] = useState<countriesListType>([]);
+// const [query, setQuery] = useState<string>("");
+// const [searchResult, setSearchResult] = useState<countriesListType>([]);
+// const [filterRegion, setFilterRegion] = useState<string>("Filter by Region");
+// const [dark, setDark] = useState(false);
+
+type countriesListType = Array<{
+  name: string;
+  capital: string | undefined;
+  region: string;
+  population: number;
+  flag: string;
+  independent: boolean;
+  alpha3Code: string;
+}>;
+
+const initialState = {
+  countriesList: <countriesListType>[],
+  query: "",
+  searchResult: "",
+  filterRegion: "Filter by Region",
+  dark: false,
+};
+
+export const countriesSlice = createSlice({
+  name: "countries",
+  initialState,
+  reducers: {
+    setDark: (state) => {
+      state.dark = !state.dark;
+    },
+  },
+});
