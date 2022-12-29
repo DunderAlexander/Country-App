efbd636595027159166b86c9fce5de68bc0a4489
DunderAlexander <mego.sanello@gmail.com>
2022-12-29 22:04:36 +0400

    dark mode has dynamically changed name

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMoon } from "@fortawesome/free-solid-svg-icons";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setDark } from "../redux/slices/countriesSlice";
+import { RootState } from "../redux/store";
 
 export const Header = () => {
+  const { dark } = useSelector((state: RootState) => state.countries);
   const dispatch = useDispatch();
   return (
     <div className="flex flex-row justify-between items-center bg-White dark:bg-DarkBlue py-8 px-16 shadow-md text-VeryDarkBlue_LM dark:text-White font-semibold text-lg">
@@ -14,7 +16,7 @@ export const Header = () => {
           style={{ marginRight: "1rem", rotate: "-30deg", cursor: "pointer" }}
           onClick={() => dispatch(setDark())}
         />
-        Dark Mode
+        {dark ? "Light mode" : "Dark mode"}
       </span>
     </div>
   );
